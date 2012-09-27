8137f0e4d00516468023921066dda5ae949849a9
Guilherme <guilherme@reiscampos.com.br>
2012-09-26 22:27:58 -0300

    changing color

--- PrimeiroProjetoCSharp/Form1.Designer.cs
+++ PrimeiroProjetoCSharp/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.label1.Location = new System.Drawing.Point(100, 111);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 13);
@@ -50,7 +50,6 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
