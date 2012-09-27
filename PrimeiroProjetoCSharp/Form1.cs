2e8f8d727b245c9e2fe4158eff9a571cb1273520
Guilherme <guilherme@reiscampos.com.br>
2012-09-26 22:31:36 -0300

    updating Form1.cs

--- PrimeiroProjetoCSharp/Form1.cs
+++ PrimeiroProjetoCSharp/Form1.cs
@@ -15,5 +15,10 @@ namespace PrimeiroProjetoCSharp
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
